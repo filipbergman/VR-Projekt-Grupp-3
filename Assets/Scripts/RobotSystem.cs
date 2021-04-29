11fd23db0f13127093c331389e757db339a593d0
ElmerDellson <elmer@dellson.com>
2021-04-29 10:40:40 +0200

    Added Robot System

---
+++ Assets/Scripts/RobotSystem.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotSystem : MonoBehaviour
+{
+    [Header("General")]
+    public float moveSpeed = 1f;
+    public float acceleration = 1f;
+    public float turnSpeed = 1f;
+    public float fieldOfView = 45;
+    public float sightDist = 100f;
+
+    [Header("Settings")]
+    [Tooltip("The actual GameObject that the robot will target. Must have a collider.")]
+    public GameObject targetPlayer;
+    public GameObject[] controlPoints;
+
+    [Header("Tweaks")]
+    public float decelerationDistance = 1f;
+    public float controlPointDistBias = 0.1f;
+    public float controlPointRotBias = 1f;
+    public float stoppedSpeedBias = 0.2f;
+
+    [Header("Debug")]
+    public float currentSpeed;
+    public bool showRay = false;
+    public bool showControlPoints = false;
+    
+    private GameObject indicatorSphere;
+    
+    private Transform robotTransform;
+    private int currentControlPointIndex = 0;
+    private GameObject currentControlPoint;
+    private bool forwardDirection;
+    private bool isRotating = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        robotTransform = gameObject.transform.Find("Robot");
+
+        if (controlPoints.Length < 1)
+            Debug.LogError("Please add at least one Control Point!");
+        else
+        {
+            currentControlPoint = controlPoints[currentControlPointIndex];
+
+            if (!showControlPoints)
+            {
+                foreach (GameObject cp in controlPoints)
+                    cp.transform.localScale = Vector3.zero;
+            }
+        }
+
+        indicatorSphere = gameObject.transform.Find("Robot").Find("IndicatorSphere").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+
+        HandleDetection();
+    }
+
+    private void HandleMovement()
+    {
+        //Don't move the robot if it is rotating
+        if (!isRotating)
+        {
+            float speed = moveSpeed;
+
+            if (Vector3.Distance(robotTransform.position, currentControlPoint.transform.position) < decelerationDistance)
+            {
+                speed = 0;
+
+                if (currentSpeed < stoppedSpeedBias)
+                    UpdateControlPoint();
+            }
+
+            Vector3 direction = (currentControlPoint.transform.position - robotTransform.position).normalized;
+
+            currentSpeed = Mathf.Lerp(currentSpeed, speed, acceleration * Time.deltaTime);
+
+            robotTransform.position += direction * currentSpeed * Time.deltaTime;
+        }
+        //Rotate
+        else
+        {
+            Quaternion currentRobotRotation = robotTransform.rotation;
+            Quaternion targetRobotRotation = currentRobotRotation * Quaternion.FromToRotation(robotTransform.forward, currentControlPoint.transform.position - robotTransform.position);
+
+            robotTransform.rotation = Quaternion.RotateTowards(currentRobotRotation, targetRobotRotation, turnSpeed * Time.deltaTime);
+
+            //Stop rotating when the robot is facing the current control point
+            if (Math.Abs(currentRobotRotation.eulerAngles.y - targetRobotRotation.eulerAngles.y) < controlPointRotBias)
+                isRotating = false;
+        }
+    }
+
+    private void HandleDetection()
+    {
+        RaycastHit hit;
+
+        Vector3 toPlayer = (targetPlayer.transform.position - robotTransform.position) + (Vector3.up * 0.5f);
+        float angleToPlayer = Vector3.Angle(robotTransform.forward, toPlayer);
+
+        if (angleToPlayer < fieldOfView)
+        {
+            if (Physics.Raycast(robotTransform.position, toPlayer, out hit, sightDist))
+            {
+                //DEBUG-------
+                if (showRay)
+                    Debug.DrawRay(robotTransform.position, toPlayer, Color.red);
+                //------------
+
+                if (hit.collider.gameObject.transform.root.name == "Player")
+                {
+                    indicatorSphere.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.yellow);
+
+                    PlayerSpotted();
+                }
+
+                //DEBUG-------
+                else
+                {
+                    indicatorSphere.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.black);
+                }
+                //------------
+            }
+        }
+
+        //DEBUG-------
+        else
+        {
+            indicatorSphere.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.black);
+        }
+        //------------
+    }
+
+    private void PlayerSpotted()
+    {
+        Debug.Log("Player spotted!");
+    }
+
+    private void UpdateControlPoint()
+    {
+        //Go to next control point unless the robot is at either end of the path
+        //If it is, switch direction
+        if (forwardDirection)
+        {
+            if (currentControlPointIndex >= controlPoints.Length - 1)
+            {
+                forwardDirection = false;
+                currentControlPointIndex--;
+            } else 
+                currentControlPointIndex++;
+        } 
+        else
+        {
+            if (currentControlPointIndex <= 0)
+            {
+                forwardDirection = true;
+                currentControlPointIndex++;
+            }
+            else
+                currentControlPointIndex--;
+        }
+
+        //Actually update control point
+        currentControlPoint = controlPoints[currentControlPointIndex];
+
+        //Start rotating towards the new control point
+        isRotating = true;
+    }
+}
