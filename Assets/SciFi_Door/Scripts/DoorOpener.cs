77df4ba35775f49e686a792b75adad41a76242af
d <d>
2021-05-06 16:51:04 +0200

    Level1 started

---
+++ Assets/SciFi_Door/Scripts/DoorOpener.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OpenDoor()
+    {
+            animator.SetBool("isOpening", true);    
+    }
+
+    public void CloseDoor() {
+        
+           animator.SetBool("isOpening", false);
+        }
+
+    }
---
+++ Assets/SciFi_Door/Scripts/DoorTriggerButton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTriggerButton : MonoBehaviour
+{
+    [SerializeField] private DoorOpener door;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            door.OpenDoor();
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            door.CloseDoor();
+        }
+    }
+
+}
