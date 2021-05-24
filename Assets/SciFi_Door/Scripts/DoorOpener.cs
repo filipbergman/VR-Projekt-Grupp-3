b2f2f671d2f0b5206da230474fc236b5497e86e5
g <g>
2021-05-24 17:03:14 +0200

    Small fixes

--- Assets/SciFi_Door/Scripts/DoorOpener.cs
+++ Assets/SciFi_Door/Scripts/DoorOpener.cs
@@ -6,25 +6,25 @@ public class DoorOpener : MonoBehaviour
 {
     private Animator animator;
     public AudioClip doorSound;
-    private AudioSource audio;
+    private AudioSource audioSpeaker;
 
     // Start is called before the first frame update
     private void Awake()
     {
         animator = GetComponent<Animator>();
-        audio = GetComponentInChildren<AudioSource>();
+        audioSpeaker = GetComponent<AudioSource>();
     }
 
     public void OpenDoor()
     {
         Debug.Log("OPEN");
-        audio.PlayOneShot(doorSound, 1f);
+        audioSpeaker.PlayOneShot(doorSound, 1f);
         animator.SetBool("isOpening", true);   
     }
 
     public void CloseDoor()
     {
-        audio.PlayOneShot(doorSound, 1f);
+        audioSpeaker.PlayOneShot(doorSound, 1f);
         animator.SetBool("isOpening", false);
     }
 }
